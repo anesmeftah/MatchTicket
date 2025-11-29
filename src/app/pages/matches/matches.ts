441327b1cc2cd2858f54715e021dd443bd809273
anasmeftah <anesmeftah03@gmail.com>
2025-11-29 12:36:32 +0100

    feat: update matches and tickets pages to include sidebar and topbar components

--- src/app/pages/matches/matches.ts
+++ src/app/pages/matches/matches.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { TopbarComponent } from '../../components/topbar/topbar';
 
 @Component({
   selector: 'app-matches',
-  imports: [],
+  imports: [Sidebar , TopbarComponent],
   templateUrl: './matches.html',
   styleUrl: './matches.css',
 })
--- src/app/pages/tickets/tickets.ts
+++ src/app/pages/tickets/tickets.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { TopbarComponent } from '../../components/topbar/topbar';
+import { StatisticsCards } from '../../components/statistics-cards/statistics-cards';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [Sidebar , TopbarComponent],
   templateUrl: './tickets.html',
   styleUrl: './tickets.css',
 })
